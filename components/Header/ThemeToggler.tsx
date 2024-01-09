42ac739a57405f34da7a8eef843736c64217fc1f
argenestel <arpitpali68@gmail.com>
2024-01-09 09:45:19 +0530

    do

--- components/Header/ThemeToggler.tsx
+++ components/Header/ThemeToggler.tsx
@@ -5,27 +5,27 @@ const ThemeToggler = () => {
   const { theme, setTheme } = useTheme();
 
   return (
-    // <button
-    // aria-label="theme toggler"
-    //   onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-    //   className="bg-gray-2 dark:bg-dark-bg flex cursor-pointer items-center justify-center rounded-full text-black dark:text-white mr-1.5 absolute lg:static right-17"
-    // >
-    //   <Image
-    //     src="/images/icon/icon-moon.svg"
-    //     alt="logo"
-    //     width={21}
-    //     height={21}
-    //     className="dark:hidden"
-    //   />
+    <button
+    aria-label="theme toggler"
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      className="bg-gray-2 dark:bg-dark-bg flex cursor-pointer items-center justify-center rounded-full text-black dark:text-white mr-1.5 absolute lg:static right-17"
+    >
+      <Image
+        src="/images/icon/icon-moon.svg"
+        alt="logo"
+        width={21}
+        height={21}
+        className="dark:hidden"
+      />
 
-    //   <Image
-    //     src="/images/icon/icon-sun.svg"
-    //     alt="logo"
-    //     width={22}
-    //     height={22}
-    //     className="hidden dark:block"
-    //   />
-    // </button>
+      <Image
+        src="/images/icon/icon-sun.svg"
+        alt="logo"
+        width={22}
+        height={22}
+        className="hidden dark:block"
+      />
+    </button>
   );
 };
 
