752cd39c782b9430fcc2effc28e95b400e0cc904
argenestel <arpitpali68@gmail.com>
2024-01-13 10:03:18 +0530

    Fixes: hidder

--- components/Brands/SingleBrand.tsx
+++ components/Brands/SingleBrand.tsx
@@ -9,34 +9,18 @@ const SingleBrand = ({ brand }: { brand: Brand }) => {
 
   return (
     <>
-      <motion.a
-        variants={{
-          hidden: {
-            opacity: 0,
-            y: -20,
-          },
-
-          visible: {
-            opacity: 1,
-            y: 0,
-          },
-        }}
-        initial="hidden"
-        whileInView="visible"
-        transition={{ duration: 1, delay: id }}
-        viewport={{ once: true }}
-        href={href}
+      <div
         className="animate_top block relative h-10 mx-w-full w-[98px]"
       >
         <Image
-          className="opacity-100 transition-all duration-300 hover:opacity-90 dark:hidden"
-          src={image}
+          className="opacity-100 transition-all duration-300 hover:opacity-90"
+          src={imageLight}
           alt={name}
           fill
           
         />
 
-      </motion.a>
+      </div>
     </>
   );
 };
