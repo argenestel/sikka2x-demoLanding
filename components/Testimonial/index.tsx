3069842916775b95b341bc321539ba90b77523f9
argenestel <arpitpali68@gmail.com>
2024-01-15 15:44:31 +0530

    Fix

--- components/Testimonial/index.tsx
+++ components/Testimonial/index.tsx
@@ -1,60 +1,45 @@
-"use client";
+"use client"
 import React from "react";
 import SectionHeader from "../Common/SectionHeader";
-
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Pagination } from "swiper";
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-
-import { motion } from "framer-motion";
 import Image from "next/image";
-
 import { testimonials } from "./testimonialData";
 import StarRating from "./Starrating";
 
+// Testimonial component
 const Testimonial = () => {
   return (
-
-      <section className="py-8 dark:bg-gradient-to-b from-transparent via-sikkaMaroon to-transparent">
-        <div className="mx-auto max-w-c-1315 px-4 md:px-8 xl:px-0 ">
-          {/* <!-- Section Title Start --> */}
-          <div className="animate_top text-center mx-auto">
-            <SectionHeader
-              headerInfo={{
-                title: `Reviews`,
-                subtitle: `Our Trusted Users`,
-                description: `What our users say about us`,
-              }}
-            />
-          </div>
-
-          {/* <!-- Section Title End --> */}
+    <section className="py-8 dark:bg-gradient-to-b from-transparent via-sikkaMaroon to-transparent">
+      <div className="mx-auto max-w-c-1315 px-4 md:px-8 xl:px-0">
+        {/* Section Title */}
+        <div className="animate_top text-center mx-auto">
+          <SectionHeader
+            headerInfo={{
+              title: "Reviews",
+              subtitle: "Our Trusted Users",
+              description: "What our users say about us",
+            }}
+          />
         </div>
-
-        <div
-
-          className="mx-auto max-w-c-1235 px-4 md:px-8 xl:px-0 mt-5 xl:mt-10"
-        >
-          {/* <!-- Slider main container --> */}
-
+        {/* Slider Container */}
+        <div className="mx-auto max-w-c-1235 px-4 md:px-8 xl:px-0 mt-5 xl:mt-10">
           <div className="swiper testimonial-01 pb-4 mb-8">
-          <div className="flex md:flex-row flex-col justify-center items-center">
-            {/* Existing rating image */}
-            <div className="md:w-1/2">
-              <div className="justify-center pb-4 rounded-sm">
-                <Image 
-                  src="/images/about/starRating.png"
-                  alt="starRating"
-                  width={200}
-                  height={200}
-                  className="rounded-lg w-full md:hidden xl:hidden"
-                />
+            <div className="flex md:flex-row flex-col justify-center items-center">
+              {/* Star Rating Image */}
+              <div className="md:w-1/2">
+                <div className="justify-center pb-4 rounded-sm">
+                  <Image
+                    src="/images/about/starRating.png"
+                    alt="starRating"
+                    width={200}
+                    height={200}
+                    className="rounded-lg w-full md:hidden xl:hidden"
+                  />
+                </div>
               </div>
             </div>
-            </div>
-            {/* <!-- Additional required wrapper --> */}
+            {/* Testimonial Swiper */}
             <Swiper
               spaceBetween={50}
               slidesPerView={3}
@@ -67,40 +52,35 @@ const Testimonial = () => {
               }}
               modules={[Autoplay, Pagination]}
               breakpoints={{
-                // when window width is >= 640px
                 0: {
                   slidesPerView: 1,
                 },
-                // when window width is >= 768px
                 768: {
                   slidesPerView: 3,
                 },
               }}
             >
+              {/* Testimonial Slides */}
               {testimonials.map((testimonial, index) => (
                 <SwiperSlide key={index}>
                   <div className="bg-white rounded-lg shadow-solid-9 dark:shadow-none dark:bg-blacksection dark:border dark:border-strokedark p-9 pt-7.5">
                     <div className="flex justify-between border-b border-stroke dark:border-strokedark pb-6 mb-7.5">
                       <div>
-                        
                         <h4 className="flex text-black dark:text-white text-metatitle3 mb-1.5">
-                        <Image
-                        width={40}
-                        height={40}
-                        className=" flex-auto"
-                        src={`./images/user/user-01.svg`}
-                        alt="User"
-                      />
-                          <div className="ml-2 mt-2">{testimonial.user}</div> 
+                          <Image
+                            width={40}
+                            height={40}
+                            className="flex-auto"
+                            src={`./images/user/user-01.svg`}
+                            alt="User"
+                          />
+                          <div className="ml-2 mt-2">{testimonial.user}</div>
                         </h4>
-                        {/* <p>{testimonial.role}</p> */}
-                        
                       </div>
                       <div className="mt-2">
-                      <StarRating rating={testimonial.rating} />
+                        <StarRating rating={testimonial.rating} />
                       </div>
                     </div>
-          
                     <p>{testimonial.content}</p>
                   </div>
                 </SwiperSlide>
@@ -108,8 +88,8 @@ const Testimonial = () => {
             </Swiper>
           </div>
         </div>
-      </section>
- 
+      </div>
+    </section>
   );
 };
 
