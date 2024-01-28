91635bac97d3fbaa6a2f8d6e9a8e5f227ea3295d
argenestel <arpitpali68@gmail.com>
2024-01-28 16:07:01 +0530

    brand fix

--- components/Brands/index.tsx
+++ components/Brands/index.tsx
@@ -16,13 +16,13 @@ const Brands = () => {
     }}
   />
   <div className="mx-auto max-w-c-1390 px-4 md:px-8 2xl:px-0">
-    <div className="grid grid-cols-3 md:grid-cols-6 gap-7.5 lg:gap-12.5 xl:gap-29 justify-center items-center">
+    {/* <div className="grid grid-cols-3 md:grid-cols-6 gap-7.5 lg:gap-12.5 xl:gap-29 justify-center items-center">
       {brandData.map((brand, key) => (
         <React.Suspense fallback={<a>Loading...</a>}>
           <SingleBrand brand={brand} key={key}/>
         </React.Suspense>
       ))}
-    </div>
+    </div> */}
   </div>
 </section>
   );
