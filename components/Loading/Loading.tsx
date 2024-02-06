76724923908fab1e001a1affd786e58580a04c5b
argenestel <arpitpali68@gmail.com>
2024-02-06 19:44:42 +0530

    some changes

--- components/Loading/Loading.tsx
+++ components/Loading/Loading.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
 const LoadingPage: React.FC = () => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationData,
-  };
+
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
